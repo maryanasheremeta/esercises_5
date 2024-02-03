7d993408f5d39b3bc43b98153a4d7d7e942f42c8
maryanasheremeta <maryanasc96@gmail.com>
2024-02-03 20:23:20 +0100

    esercizio sulle Strings

--- exercises_5_1.py
+++ exercises_5_1.py
@@ -1,21 +1,16 @@
 the_text= input("Enter String:")
-
-kwlist = {*'''!"#$%&'()*+,-./:;<=>?@[\]^`{|}~'''}
+kw_list = {*'''!"#$%&'()*+,-./:;<=>?@[\]^`{|}~'''}
 ban = __import__('keyword').iskeyword
 is_valid = True
 if not the_text[0].isnumeric() and not ban(the_text):
     for sym in the_text:
-        if sym.isupper() or sym in kwlist:
+     if sym.isupper() or sym in kw_list:
             is_valid = False
             break
 else:
     is_valid = False
 print(is_valid)
 
-import keyword
-name=input("Enter variables :")
-if name.isidentifier()and name [0]!="_"and name.count("_")<=1 and name not in keyword.kwlist:
-    print(True)
-else:
-    print(False)
+
+
 
