3fd1c13f15ccfda04bd093c66bf8a1b7626d735c
maryanasheremeta <maryanasc96@gmail.com>
2024-02-03 19:29:17 +0100

    esercizio sulle Strings

--- exercises_5_1.py
+++ exercises_5_1.py
@@ -1,6 +1,6 @@
 import keyword
 name=input("Enter variables :")
-if name.isidentifier()and name [0]!="_"and name.count("_")<=1 and name not in keyword.kwlist
+if name.isidentifier()and name [0]!="_"and name.count("_")<=1 and name not in keyword.kwlist:
     print(True)
 else:
     print(False)
