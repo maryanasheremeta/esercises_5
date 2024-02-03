fb21484966ce93347f8b45b37fed788f565eaae0
maryanasheremeta <maryanasc96@gmail.com>
2024-02-03 17:34:24 +0100

    esercizio sulle Strings

---
+++ exercises_5_1.py
@@ -0,0 +1,7 @@
+import keyword
+name=input("Enter variables :")
+if name.isidentifier()and name [0]!="_"and name.count("_")<=1 and name not in keyword.kwlist
+    print(True)
+else:
+    print(False)
+
